1e79b47422d992eef5629d670435c67fa3ce93d2
Owen Price Skelly <owen@Owens-MacBook-Pro.local>
2021-10-11 16:16:39 -0500

    naive implementation of parse done

--- lib/lib.ml
+++ lib/lib.ml
@@ -8,16 +8,43 @@ open Prelude
 
 (* library code for attachment converter goes here *)
 module type CONVERT =
-  sig
-    type filepath
-    type parsetree
-    val parse : string -> parsetree
-    val amap : ('a -> 'b) -> parsetree -> parsetree
-    val acopy : ('a -> 'b) -> parsetree -> parsetree
-    val to_string : parsetree -> string
-    val convert : filepath -> string -> string
-    val acopy_email : string -> (string -> string) -> string
-  end
+sig
+  type filepath
+  type parsetree
+  val parse : string -> parsetree
+  val amap : ('a -> 'b) -> parsetree -> parsetree
+  val acopy : ('a -> 'b) -> parsetree -> parsetree
+  val to_string : parsetree -> string
+  val convert : filepath -> string -> string
+  val acopy_email : string -> (string -> string) -> string
+end
+
+module Conversion_ocamlnet : CONVERT = struct
+  include Netmime
+  include Netmime_channels
+  include Netchannels
+  include Netstream
+
+  type filepath = string (* String.t *)
+  type parsetree = Netmime.complex_mime_message
+
+  let parse s =
+    s |>
+    new Netchannels.input_string |>
+    new Netstream.input_stream |>
+    Netmime_channels.read_mime_message
+
+  (* see http://projects.camlcity.org/projects/dl/ocamlnet-4.1.9/doc/html-main/Netmime_tut.html *)
+
+
+
+
+  let amap = assert false
+  let acopy = assert false
+  let to_string = assert false
+  let convert = assert false
+  let acopy_email = assert false
+end
 
 
 (*
