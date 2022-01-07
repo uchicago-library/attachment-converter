698c1618da6191e2f772da9a39f4d9901a193c60
Matt Teichman <teichman@uchicago.edu>
2022-01-07 11:59:21 -0600

    opam environment variables

--- get-started.sh
+++ get-started.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 opam switch create 4.12.0
+eval $(opam env)
 opam install ocamlbuild ocamlfind dune
 opam pin add -k hg prelude https://www.lib.uchicago.edu/keith/hg/prelude
 opam pin add mattlude https://github.com/bufordrat/mattlude.git
