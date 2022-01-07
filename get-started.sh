8088a8de51c9bd2a6049366213ede9cdb8833f79
Matt Teichman <teichman@uchicago.edu>
2022-01-07 12:04:26 -0600

    dash y options in pins

--- get-started.sh
+++ get-started.sh
@@ -2,7 +2,7 @@
 opam switch create 4.12.0 &&
 eval $(opam env) &&
 opam install ocamlbuild ocamlfind dune &&
-opam pin add -k hg prelude https://www.lib.uchicago.edu/keith/hg/prelude &&
-opam pin add mattlude https://github.com/bufordrat/mattlude.git &&
-opam pin add spinup https://github.com/bufordrat/spinup.git
+opam pin -y add -k hg prelude https://www.lib.uchicago.edu/keith/hg/prelude &&
+opam pin -y add mattlude https://github.com/bufordrat/mattlude.git &&
+opam pin -y add spinup https://github.com/bufordrat/spinup.git
 
