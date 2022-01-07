7d61df732e1a501078f8a409d80e456604be0a04
Matt Teichman <teichman@uchicago.edu>
2022-01-07 12:01:11 -0600

    ampersandify

--- get-started.sh
+++ get-started.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-opam switch create 4.12.0
-eval $(opam env)
-opam install ocamlbuild ocamlfind dune
-opam pin add -k hg prelude https://www.lib.uchicago.edu/keith/hg/prelude
-opam pin add mattlude https://github.com/bufordrat/mattlude.git
+opam switch create 4.12.0 &&
+eval $(opam env) &&
+opam install ocamlbuild ocamlfind dune &&
+opam pin add -k hg prelude https://www.lib.uchicago.edu/keith/hg/prelude &&
+opam pin add mattlude https://github.com/bufordrat/mattlude.git &&
 opam pin add spinup https://github.com/bufordrat/spinup.git
 
