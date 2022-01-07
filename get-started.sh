341a16a51fd32a9e46dbcf8e70ff4511a285b9de
Matt Teichman <teichman@uchicago.edu>
2022-01-07 11:50:13 -0600

    add shell script to help with onboarding email archives fellows

---
+++ get-started.sh
@@ -0,0 +1,6 @@
+opam switch create 4.12.0
+opam install ocamlbuild ocamlfind dune
+opam pin add -k hg prelude https://www.lib.uchicago.edu/keith/hg/prelude
+opam pin add mattlude https://github.com/bufordrat/mattlude.git
+opam pin add spinup https://github.com/bufordrat/spinup.git
+
