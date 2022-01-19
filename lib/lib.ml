4af0a5716c72bf8a03a5dc18fd0ba81e7209ee83
Owen Price Skelly <owen@Owens-MacBook-Pro.local>
2022-01-19 16:51:19 -0600

    end of day 1/19/22

--- lib/lib.ml
+++ lib/lib.ml
@@ -4,6 +4,7 @@
  * Distributed under the ISC license, see terms at the end of the file.
 *)
 
+
 open Prelude
 
 (* library code for attachment converter goes here *)
@@ -17,11 +18,11 @@ sig
   val amap : htransform -> btransform -> parsetree -> parsetree
   val acopy : htransform -> btransform -> parsetree -> parsetree
   val to_string : parsetree -> string
-  val convert : filepath -> (string -> string)
+  val convert : filepath list -> (string -> string)
   val acopy_email : string -> (string -> string) -> string
 end
 
-module Conversion_ocamlnet (*: CONVERT*) = struct
+module Conversion_ocamlnet (* : CONVERT *) = struct
 
   type htransform = string -> string
   type btransform = string -> string
@@ -37,6 +38,17 @@ module Conversion_ocamlnet (*: CONVERT*) = struct
      -- I /think/ that with_in_obj_channel should close both the Netchannels and the Netstream input bits,
      but it's worth keeping an eye on. *)
 
+
+  let field_params_alist header fieldname =
+    match tail (String.cuts ~sep:";" (header#field fieldname)) with
+      Some [] -> [(fieldname, (header#field fieldname))] (* Case 1: no params, alist is just field name/value *)
+    | Some params -> let kvpairs = map (String.cut ~sep:"=") params in (* case 2: split on "=" then zip together as alist*)
+      map (function (key, Some v) -> (key, v) | (key, None) -> (key, "")) kvpairs
+    | None -> assert false
+
+  (*let get_parts = function `Parts plst -> plst | _-> assert false;;*)
+
+
   let header_to_string (h : Netmime.mime_header) =
     let buf = Stdlib.Buffer.create 1024 in
     Netchannels.with_out_obj_channel
@@ -50,6 +62,21 @@ module Conversion_ocamlnet (*: CONVERT*) = struct
          (new Netchannels.input_string s)
          (fun ch -> Netmime_string.read_header ?downcase:(Some false) (new Netstream.input_stream ch)))
 
+  (* param_map takes a builds a string -> string function from a specified header field (e.g. "Content-Disposition"),
+   * a parameter that we might find in that field (e.g. "filename"), and a
+   * simpler string->string function that just targets that value (e.g. a
+   * change_extension function or something.)
+  *)
+  (* let param_map = () *)
+
+
+  (* let mk_header  = assert false *)
+
+  (* Notes:
+     Content-Disposition headers provide information about how to present a
+     message or a body part. When a body part is to be treated as an attached
+     file, the Content-Disposition header will include a file name parameter. *)
+
 
   let rec amap f g (tree : parsetree) =
     match tree with
@@ -61,6 +88,14 @@ module Conversion_ocamlnet (*: CONVERT*) = struct
       else (header_from_string (f h), `Body (b#set_value (g b#value); b))
     | (header, `Parts p_lst) ->
       (header, `Parts (List.map (amap f g) p_lst))
+
+  let rec header_alists (t: parsetree) =
+    match t with
+      (h, `Body _) ->
+      let parse_rest (fname, vals) =
+        (fname, Netmime_string.scan_value_with_parameters_ep vals [])
+      in map parse_rest h#fields
+    | (_, `Parts p_lst) -> List.concat_map header_alists p_lst
 
   let rec acopy f g (tree : parsetree) =
     match tree with
@@ -82,8 +117,6 @@ module Conversion_ocamlnet (*: CONVERT*) = struct
 
       in (header, `Parts List.(concat_map copy_or_skip p_lst))
 
-
-
   let to_string (tree : parsetree) =
     let (header, _) = tree in
     let n = try Netmime_header.get_content_length header
@@ -100,17 +133,106 @@ module Conversion_ocamlnet (*: CONVERT*) = struct
   let convert =
     Prelude.Unix.Proc.rw ?oknon0:(Some false)  ?env:None ?usepath:(Some true)
 
+  let is_attachment (tree : parsetree) =
+    let (header, _) = tree in
+    let s = try header # field "content-disposition"
+      with Not_found -> ""
+    in Strings.prefix "attachment" (String.lowercase_ascii s)
+
+  let unparts pts =
+    match pts with
+      `Parts plist -> plist
+    | _ -> assert false
+
+  let unbody bdy =
+    match bdy with
+      `Body b -> b
+    | _ -> assert false
+
+  let xmas_tree () =
+    let (_, parts) = parse (readfile "../2843") in
+    match unparts parts with
+      (_ :: attached :: _ ) -> attached
+    | _ -> assert false
+
+  let update_filename hstr ?(ext="") =
+    let open Strings in
+    let open Filename in
+    match substr "filename=" hstr with
+      Some lo ->
+      let hi = lo + try Option.get (substr "\r\n" hstr#.(lo,0)) with
+          Invalid_argument _ -> assert false (* should never happen if libpst does its job *)
+      in
+      let old_name =    (* offsets are for escaped quotation marks and/or a ';',
+                           which is only required if more params follow the filename param.*)
+        if mem ';' hstr#.(lo, hi)
+        then hstr#.(lo + 10, hi - 2)
+        else hstr#.(lo + 10, hi - 1)
+      in
+      let new_name = (remove_extension old_name)
+                     ^ ".CONVERTED.yyyymmdd"
+                     ^ (extension old_name)
+                     ^ ext
+      in replace old_name new_name hstr
+    | _ -> assert false
+
+  (* TESTS *)
+  let pdf_convert_test fname =
+    let header_func hstring =
+      let hs = String.lowercase_ascii hstring in
+      match Strings.(
+          (substr "content-disposition: attachment;" hs,
+           substr "content-type: application/pdf" hs)
+        )
+      with
+        (Some _, Some _) ->
+        update_filename hstring ~ext:""
+      | _ -> hstring (* noop when not a pdf and attachment *)
+    in
+    let body_func _bstring =
+      "[REDACTED]" (*todo call convert with some utility*)
+    in
+    let tree = parse (readfile fname) in
+    let converted_tree = amap header_func body_func tree
+    in writefile ~fn:(fname ^ "_xmas_cancelled") (to_string converted_tree)
+
+
   let upcase_header_and_delete_body fname =
     let f = String.uppercase_ascii in
     let g = fun _ -> "" in
     let tree = parse (Prelude.readfile fname) in
     Prelude.writefile ~fn:(fname ^ "_upcased_and_deleted") (tree |> (amap f g) |> to_string)
 
-    (* let acopy_email = assert false *)
+  let omit_gore_y_details fname =
+    let f = Fun.const "Content-Type: application/json\r\n\r\n" in
+    (* let f = fun _ -> "From: Bill Clinton <president@whitehouse.gov>\r\n\r\n" in *)
+    let g = Fun.const "" in
+    let tree = parse (Prelude.readfile fname) in
+    Prelude.writefile ~fn:(fname ^ "_contented") (tree |> (amap f g) |> to_string)
+
+  let acopy_email () = assert false
+
+
 
 end
 
 
+
+(*
+ * Copyright (c) 2021 Matt Teichman
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
 
 
 module Practicum = struct
@@ -280,11 +402,11 @@ end
 
 (*
  * Copyright (c) 2021 Matt Teichman
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -293,4 +415,3 @@ end
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
-
