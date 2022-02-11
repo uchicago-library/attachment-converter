cfa7e8259dfefa267c0d126f158b16d878228314
OwenPriceSkelly <Owen.Price.Skelly@gmail.com>
2022-02-11 14:28:34 -0600

    optional `eol` arg for `to_mbox` (not always crlf)

--- lib/lib.ml
+++ lib/lib.ml
@@ -255,9 +255,14 @@ module REPLTesting = struct
     | `Body b -> b
     | _ -> assert false
 
-  let to_mbox ?(escape=false)  =
-    let fromline = "From jorge@babel.lib Thu Aug 24 12:00:00 1899\n" in
-    Lists.foldl (fun acc email -> acc ^ fromline ^ email ^"\n") ""
+  let to_mbox ?(escape=false) ?(eol="\n") =
+    let fromline = "From jorge@babel.lib Thu Aug 24 12:00:00 1899" ^ eol in
+    if escape then
+      let open Strings in
+      let escape_froms = replace (eol ^ "From ") (eol ^ ">From ") << replace (eol ^ ">From ") (eol ^ ">>From ") in
+      fold_left (fun acc email -> acc ^ fromline ^ email ^ eol) "" << map escape_froms
+    else
+      fold_left (fun acc email -> acc ^ fromline ^ email ^ eol) ""
 
 
 
