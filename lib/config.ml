bd0be692b6cecb35149d5f8835e16b03ed9a961d
Nathan Mull <nmmull@Nathans-MacBook-Air.local>
2022-02-23 18:34:06 -0600

    Large scale changes of Formats

--- lib/config.ml
+++ lib/config.ml
@@ -1,34 +1,30 @@
 open Prelude
 
 module Formats = struct
-
-  type htransform = string -> string
-  type dtransform = string -> string
-
   type variety =
-    | DataOnly of dtransform
-    | DataAndHeader of (htransform * dtransform)
+    | DataOnly
+    | DataAndHeader
     | NoChange
+
+  type transform_data =
+    { target_type: string;
+      shell_script: string;
+      variety: variety
+    }
 
   module Dict = Map.Make (String)
 
-  type t = (variety list) Dict.t
+  type t = (transform_data list) Dict.t
 
   module Error = struct
     type t =
       | ReferParse of string
       | ConfigData of string
-      | Unix of (string * Unix.error)
-      | MimeParse of string
-      | CharacterEncoding of string
 
     let map_msg f err =
       match err with
-      | ReferParse msg        -> ReferParse (f msg)
-      | ConfigData msg        -> ConfigData (f msg)
-      | Unix (msg, unix)      -> Unix (f msg, unix)
-      | MimeParse msg         -> MimeParse (f msg)
-      | CharacterEncoding msg -> CharacterEncoding (f msg)
+      | ReferParse msg -> ReferParse (f msg)
+      | ConfigData msg -> ConfigData (f msg)
   end
   type error = Error.t
 end
@@ -41,7 +37,7 @@ module ParseConfig = struct
       target_type:  string;
       shell_script: string }
 
-  let config_entry_of_assoc config_assoc =
+  let entry_of_assoc config_assoc =
     let construct_config_entry st tt ss =
       { source_type = st;
         target_type = tt;
@@ -59,36 +55,33 @@ module ParseConfig = struct
     in
     Ok (construct_config_entry st tt ss)
 
-  let variety_of_config_entry entry =
-    let header_transformer = id in (* update_mimetype entry.source_type entry.target_type in *)
-    let data_transformer = id (* convert entry.shell_script *)
+  let transform_data_of_entry entry =
+    let variety_of_entry =
+        if entry.source_type = entry.target_type
+        then DataOnly
+        else DataAndHeader
     in
-    if entry.source_type = entry.target_type then
-      DataOnly data_transformer
-    else
-      DataAndHeader (header_transformer, data_transformer)
+    { target_type = entry.source_type;
+      shell_script = entry.shell_script;
+      variety = variety_of_entry }
 
-  let parse_config_file (path_to_config: string) =
-    let ( let* ) = Result.(>>=) in
-    let open Result in
-    let insert_append k v = Dict.update k (fun curr -> Some (v :: Option.value curr ~default:[])) in
-    let config_str = Prelude.readfile path_to_config in
-    let add_line_num i = Error.map_msg (Printf.sprintf "Line %d, %s" i) in
-    let variety_dict_update line_num next accum =
-        let* entry = config_entry_of_assoc next in
-          Result.witherr (add_line_num line_num)
-            (Ok (insert_append entry.source_type (variety_of_config_entry entry) accum))
+  let parse_config_file path_to_config =
+    let (>>=) = Result.(>>=) in
+    let insert_append k v =
+      Dict.update k (fun curr -> Some (v :: Option.value curr ~default:[]))
     in
-    let collect_varieties line_num next accum_or_error = accum_or_error >>= (variety_dict_update line_num next) in
-    let error_handler line_num line accum_or_error = accum_or_error >>=
-      (fun _ ->
-        Error (Error.ReferParse (Printf.sprintf "Line %d, Refer Parse Error: Cannot parse '%s'" line_num line)))
+    let config_str = Prelude.readfile path_to_config in
+    let add_line_num i = Error.map_msg (fun msg -> Printf.sprintf "Line %d, %s" i msg) in
+    let update_accum line_num next accum =
+      Result.witherr (add_line_num line_num) (entry_of_assoc next) >>= (fun entry ->
+        Ok (insert_append entry.source_type (transform_data_of_entry entry) accum))
+    in
+    let collect_varieties line_num next raccum = raccum >>= update_accum line_num next in
+    let error_handler line_num line rerror = rerror >>= (fun _ ->
+      Error (Error.ReferParse (Printf.sprintf "Line %d, Refer Parse Error: Cannot parse '%s'" line_num line)))
     in
     Refer.fold
       (Refer.witherr error_handler collect_varieties)
       (Ok Dict.empty)
       (Refer.of_string config_str)
-
-  
-
 end
