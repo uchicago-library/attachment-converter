0598fa78a1a0102ebff18862293056e49f556539
Nathan Mull <nmmull@Nathans-MacBook-Air.local>
2022-03-09 09:35:00 -0600

    added more unit tests

--- tests/config_tests.ml
+++ tests/config_tests.ml
@@ -1,5 +1,6 @@
 open OUnit2
 open Lib.Config.ParseConfig
+open Lib.Config.Formats
 
 let check_ok f fname i =
   let description =
@@ -133,18 +134,91 @@ let wf_cs_to_data_ok         = check_ok    parse_config_str "parse_config_str" w
 let extra_cs_to_data_ok      = check_ok    parse_config_str "parse_config_str" extra_cs
 let missing_cs_to_data_error = check_error parse_config_str "parse_config_str" missing_cs
 
+let e1 =
+  { target_type   = "b"           ;
+    shell_command = "c d e"       ;
+    variety       = DataAndHeader ;
+  }
+
+let e2 =
+  { target_type   = "g"           ;
+    shell_command = "h"           ;
+    variety       = DataAndHeader ;
+  }
+
+let check_wf_cs_or_extra_cs cs =
+  let description = "checking access for wf_cs/extra_cs"       in
+  let open Lib.Config.Formats                                  in
+  let d = Result.get_ok (parse_config_str cs)                  in
+  let check key value _ = assert_equal (Dict.find key d) value in
+  description >:::
+    [ "check wf_cs first entry"  >:: check "a" [e1] ;
+      "check wf_cs second entry" >:: check "f" [e2] ;
+    ]
+
+let check_error result error =
+  let description = "error not as expected"                  in
+  let check _ = assert_equal (Result.get_error result) error in
+  description >:: check
+
+let missing_cs_error_msg =
+  check_error
+    (parse_config_str missing_cs)
+    (ConfigData
+      "Entry starting at Line 1, Config File Error: no script path given")
+
+let bad_refer_cs =
+"%source_type a
+%target_type b
+%shell_command c d e
+
+not a real line
+
+%source_type f
+%target_type g
+%shell_command h"
+
+let bad_refer_cs_msg =
+  check_error
+    (parse_config_str bad_refer_cs)
+      (ReferParse
+        "Line 5, Refer Parse Error: Cannot parse 'not a real line'")
+
+let double_entry_cs =
+"%source_type a
+%target_type b
+%shell_command c d e
+
+%source_type a
+%target_type g
+%shell_command h"
+
+let check_double_entry_cs =
+  let description = "checking access for double_entry_cs"         in
+  let open Lib.Config.Formats                                     in
+  let d = Result.get_ok (parse_config_str double_entry_cs)        in
+  let check _ = assert_equal (Dict.find "a" d) [e2; e1] in
+  description >:: check
+
+
+
 let tests = "test suite for config file parsing" >:::
-  [ wf_to_entry_ok             ;
-    extra_to_entry_ok          ;
-    nwf_to_entry_error         ;
-    wf_correct                 ;
-    extra_correct              ;
-    wf_trans_data_correct      ;
-    extra_trans_data_correct   ;
-    t_neq_s_trans_data_correct ;
-    wf_cs_to_data_ok           ;
-    extra_cs_to_data_ok        ;
-    missing_cs_to_data_error   ;
+  [ wf_to_entry_ok                   ;
+    extra_to_entry_ok                ;
+    nwf_to_entry_error               ;
+    wf_correct                       ;
+    extra_correct                    ;
+    wf_trans_data_correct            ;
+    extra_trans_data_correct         ;
+    t_neq_s_trans_data_correct       ;
+    wf_cs_to_data_ok                 ;
+    extra_cs_to_data_ok              ;
+    missing_cs_to_data_error         ;
+    check_wf_cs_or_extra_cs wf_cs    ;
+    check_wf_cs_or_extra_cs extra_cs ;
+    missing_cs_error_msg             ;
+    bad_refer_cs_msg                 ;
+    check_double_entry_cs            ;
   ]
 
 let _ = run_test_tt_main tests
