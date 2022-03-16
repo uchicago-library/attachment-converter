9853f7054b2bfb0b6ddd852a5e4c535bbedf5fdc
Nathan Mull <nmmull@Nathans-MacBook-Air.local>
2022-03-16 11:00:47 -0500

    small typos

--- lib/errorHandling.ml
+++ lib/errorHandling.ml
@@ -1,14 +1,4 @@
 
-(*
-module type Error = sig
-  type t = [> `dummy ]
-end
-
-module ErrorF (M : Error) (N : Error) : Error = struct
-  type t = [ | M.t | N.t ]
-end
-*)
-
 let message err =
   match err with
   | `DummyError     -> "Dummy error message"
