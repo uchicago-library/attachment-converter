d6871944648d0bf756d5288c35c7d6facde68e1b
Nathan Mull <nmmull@gmail.com>
2022-03-30 12:54:38 -0500

    Pretty much done

--- lib/report.ml
+++ lib/report.ml
@@ -1,13 +1,13 @@
 open Prelude
 
 let attachment_types =
-  let open Strings                                         in
-  let attach_header = "Content-Type:"                      in
-  let split_index   = len attach_header                    in
-  let attach_cont   = trimleft " " >> prefix attach_header in
-  let mime_type     = trim " " >> drop split_index         in
-  let not_multi     = not << prefix "multipart"            in
-  let open Lists                                           in
+  let open Strings                                 in
+  let attach_header = "Content-Type:"              in
+  let split_index   = len attach_header            in
+  let attach_cont   = prefix attach_header         in
+  let mime_type     = drop split_index >> trim " " in
+  let not_multi     = not << prefix "multipart"    in
+  let open Lists                                   in
   split ~sep:"\n"    >>
   filter attach_cont >>
   map mime_type      >>
