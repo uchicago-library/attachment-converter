5b54cef3e0e50d8fc06fb1fecdb45ba437a39591
Nathan Mull <nmmull@gmail.com>
2022-03-30 13:56:34 -0500

    Quick changes to exclude message?

--- lib/report.ml
+++ lib/report.ml
@@ -1,15 +1,21 @@
 open Prelude
 
-let attachment_types =
-  let open Strings                                 in
-  let attach_header = "Content-Type:"              in
-  let split_index   = len attach_header            in
-  let attach_cont   = prefix attach_header         in
-  let mime_type     = drop split_index >> trim " " in
-  let not_multi     = not << prefix "multipart"    in
-  let open Lists                                   in
-  split ~sep:"\n"    >>
-  filter attach_cont >>
-  map mime_type      >>
-  nub                >>
-  filter not_multi
+let attachment_types ?(params = false) =
+  let open Strings                                              in
+  let attach_header = "Content-Type:"                           in
+  let split_index   = len attach_header                         in
+  let attach_cont   = prefix attach_header                      in
+  let mime_type     = drop split_index >> trim " "              in
+  let just_mime     = takewhile (not << (String.contains " ;")) in
+  let not_attach = not << disjunction
+    [ prefix "multipart" ;
+      prefix "message"   ;
+    ]
+  in
+  let open Lists in
+  split ~sep:"\n"                      >>
+  filter attach_cont                   >>
+  map mime_type                        >>
+  if params then id else map just_mime >>
+  nub                                  >>
+  filter not_attach
