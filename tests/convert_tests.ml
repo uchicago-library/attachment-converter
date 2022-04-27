a1d479e1f264c180e82e6a4aae70233ed4ed4aa1
Nathan Mull <nmmull@gmail.com>
2022-04-27 18:03:48 -0500

    unit testing set up

--- tests/convert_tests.ml
+++ tests/convert_tests.ml
@@ -1,17 +1,32 @@
 open OUnit2
 open Lib.Convert.Conversion_ocamlnet
+open Prelude
 
-let empty_config_test fname =
-  let description = "empty config is noop"
-  let email   = readfile fname
+let empty_config_test_0 fname =
+  let description = "empty config is okay on email" in
+  let email   = readfile fname in
+  let check _ = assert_bool "is okay"
+    (Result.good (acopy_email
+      Lib.Configuration.Formats.Dict.empty
+      email))
+  in
+  description >:: check
+
+let empty_config_test_1 fname =
+  let description = "empty config is noop" in
+  let email   = readfile fname in
   let check _ = assert_equal
-    Ok email
+    (Ok (parse email))
     (acopy
       Lib.Configuration.Formats.Dict.empty
-      email)
+      (parse email))
+  in
+  description >:: check
+
 
 let tests = "test suite for testing conversion" >:::
-  [ empty_config_test fname  ;
+  [ empty_config_test_0 "test_emails/xmas_email" ;
+    empty_config_test_1 "test_emails/xmas_email" ;
   ]
 
 let _ = run_test_tt_main tests
