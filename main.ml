0d099f6e21a9bdb5ad0f25c8429d0ec93454bd40
Cormac <cormacduhamel@gmail.com>
2022-08-25 17:43:06 -0400

    Initital cmdliner structure

--- main.ml
+++ main.ml
@@ -7,6 +7,24 @@
  *)
 
 open Prelude
+open Cmdliner
+
+type parsed_in = (string * in_channel)
+type channel_parser = string -> (parsed_in, [`Msg of string]) Stdlib.result
+type channel_printer = parsed_in Arg.printer
+
+(*(string -> ('a, [ `Msg of string ]) Stdlib.result*)
+let channel_parser str = 
+  if Sys.file_exists str 
+  then Ok (str, (open_in str))
+  else Error (`Msg ("File: " ^ str ^ " does not exist."))
+
+(*Format.formatter -> `a -> unit*)
+let channel_printer fmt input = match input with
+  (s,_) -> Format.pp_print_string fmt s
+
+(*parsed_in conv*)
+let channel_conv = Arg.conv (channel_parser, channel_printer)
 
 let report ?(params = false) () =
   let open Lib.Report                                  in
@@ -19,14 +37,14 @@ let report ?(params = false) () =
 
 let default_config_name = ".default-config"
 
-let convert () =
+let convert ic =
   let open Lib.Convert.Conversion_ocamlnet in
   let open Lib.Configuration.ParseConfig   in
   let open Lib.ErrorHandling               in
   if   Sys.file_exists default_config_name
   then let converted_email =
          let  ( let* ) = Result.(>>=)                          in
-         let  input    = read stdin                            in
+         let  input    = read ic                               in
          let* config   = parse_config_file default_config_name in
          acopy_email config input
        in
@@ -35,17 +53,31 @@ let convert () =
        | Ok converted -> write stdout converted
   else Printf.printf "Error: missing config file '%s'\n" default_config_name
 
-(* A _very_ minimal executable *)
-let () =
-  if   Array.length Sys.argv > 1
-  then match Sys.argv.(1) with
-       | "--report"        -> report ()
-       | "--report-params" -> report ~params:true ()
-       | unknown_flag      -> Printf.printf
-                                "Error: do not recognize flag '%s'\n"
-                                unknown_flag
-  else convert ()
+(*params matched first in the case both report and params are true*)
+let convert_wrapper rpt params inp =
+  match inp with (_,ic) -> if params then report ~params:true ()
+  else if rpt then report ()
+  else convert ic
 
+let input_t = let doc = "Input file to be converted." in
+Arg.(value & pos 0 channel_conv ("STDIN", stdin) & info [] ~doc)
+
+let report_t = let doc = "Provides a list of all attachment types in a given mailbox." in
+Arg.(value & flag & info ["r"; "report"] ~doc)
+
+let report_params_t = let doc = "Prints a list of all MIME types in the input along with 
+all header and field parameters that go with it." in
+Arg.(value & flag & info ["p"; "report-params"] ~doc)
+
+let convert_t = Term.(const convert_wrapper $ report_t $ report_params_t $ input_t)
+
+let cmd = let doc = "Converts email attachments." in
+  let info = Cmd.info "convert" ~doc in
+  Cmd.v info convert_t
+
+let main () = exit (Cmd.eval cmd)
+
+let () = main ()
 (*
  * Copyright (c) 2021 Matt Teichman
  *
