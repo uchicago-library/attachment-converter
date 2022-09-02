e7c22a433fb7a8ef8bd99a527c3c7a2f36325162
Cormac <cormacduhamel@gmail.com>
2022-09-02 11:34:23 +0100

    New cmd_input type; 'report' accepts cmd_input

--- main.ml
+++ main.ml
@@ -9,27 +9,32 @@
 open Prelude
 open Cmdliner
 
-type parsed_in = (string * in_channel)
-type channel_parser = string -> (parsed_in, [`Msg of string]) Stdlib.result
-type channel_printer = parsed_in Arg.printer
+type cmd_input = [`Stdin | `File of string]
+type cmd_input_parser = string -> (cmd_input, [`Msg of string]) Stdlib.result
+type cmd_input_printer = cmd_input Arg.printer
 
 (*(string -> ('a, [ `Msg of string ]) Stdlib.result*)
-let channel_parser str = 
+let cmd_input_parser str = 
   if Sys.file_exists str 
-  then Ok (str, (open_in str))
+  then Ok (`File str)
   else Error (`Msg ("File: " ^ str ^ " does not exist."))
 
 (*Format.formatter -> `a -> unit*)
-let channel_printer fmt input = match input with
-  (s,_) -> Format.pp_print_string fmt s
+let cmd_input_printer fmt input = let str = 
+  match input with
+    | `Stdin -> "STDIN"
+    | `File fn -> fn
+  in Format.pp_print_string fmt str
 
 (*parsed_in conv*)
-let channel_conv = Arg.conv (channel_parser, channel_printer)
+let cmd_input_conv = Arg.conv (cmd_input_parser, cmd_input_printer)
 
-let report ?(params = false) () =
+let report ?(params = false) inp =
   let open Lib.Report                                  in
   let module M =  Map.Make(String)                     in
-  let types    = content_types ~params:params stdin    in
+  let types    = match inp with
+    | `File fn -> within (content_types ~params:params) fn
+    | _ -> content_types ~params:params stdin          in
   print "Content Types:";
   M.iter
     (fun k v -> print ("  " ^ k ^ " : " ^ (Int.to_string v)))
@@ -37,14 +42,16 @@ let report ?(params = false) () =
 
 let default_config_name = ".default-config"
 
-let convert ic =
+let convert inp =
   let open Lib.Convert.Conversion_ocamlnet in
   let open Lib.Configuration.ParseConfig   in
   let open Lib.ErrorHandling               in
   if   Sys.file_exists default_config_name
   then let converted_email =
          let  ( let* ) = Result.(>>=)                          in
-         let  input    = read ic                               in
+         let  input    = match inp with
+          | `File fn -> readfile fn
+          | _ -> read stdin                                    in
          let* config   = parse_config_file default_config_name in
          acopy_email config input
        in
@@ -54,20 +61,20 @@ let convert ic =
   else Printf.printf "Error: missing config file '%s'\n" default_config_name
 
 (*params matched first in the case both report and params are true*)
-let convert_wrapper rpt params inp =
-  match inp with (_,ic) -> if params then report ~params:true ()
-  else if rpt then report ()
-  else convert ic
+let convert_wrapper rpt rpt_p inp =
+  if rpt_p then report ~params:true inp
+  else if rpt then report inp
+  else convert inp
 
 let input_t = let doc = "Input file to be converted." in
-Arg.(value & pos 0 channel_conv ("STDIN", stdin) & info [] ~doc)
-
-let report_t = let doc = "Provides a list of all attachment types in a given mailbox." in
-Arg.(value & flag & info ["r"; "report"] ~doc)
+Arg.(value & pos 0 cmd_input_conv `Stdin & info [] ~doc)
 
 let report_params_t = let doc = "Prints a list of all MIME types in the input along with 
 all header and field parameters that go with it." in
 Arg.(value & flag & info ["p"; "report-params"] ~doc)
+
+let report_t = let doc = "Provides a list of all attachment types in a given mailbox." in
+Arg.(value & flag & info ["r"; "report"] ~doc)
 
 let convert_t = Term.(const convert_wrapper $ report_t $ report_params_t $ input_t)
 
