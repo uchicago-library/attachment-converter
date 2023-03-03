ea3091ccbbc2d9bc02d8bbdd9ebcf36669dcdba8
Nathan Mull <nmmull@gmail.com>
2023-03-03 17:24:02 -0600

    Some small updates to the report code

--- lib/report.ml
+++ lib/report.ml
@@ -1,7 +1,35 @@
 open Prelude
 
 (* note: should work on both mbox and individual email *)
-let content_types ?(params = false) channel =
+
+let count_line f p =
+  let open String in
+  let module M = Map.Make(String) in
+  let inc_or_zero key =
+    M.update key (fun curr -> Some (match curr with | None -> 1 | Some n -> (n + 1)))
+  in
+  let add_to_set curr line =
+    if p line
+    then inc_or_zero (f line) curr
+    else curr
+  in
+  foldlines add_to_set M.empty
+
+  let content_types ?(params = false) =
+    let content_header = "content-type:" in
+    let split_index = len content_header in
+    let f line =
+      let mime_type = drop split_index >> trim whitespace >> lowercase_ascii in
+      let just_mime = takewhile (not << (contains (whitespace ^ ";"))) in
+      if params
+      then mime_type line
+      else just_mime (mime_type line)
+    in
+    let p line = prefix content_header << lowercase_ascii in
+    count_line f p
+
+(*
+  let content_types ?(params = false) channel =
   let open String                                                              in
   let module M       = Map.Make(String)                                        in
   let content_header = "content-type:"                                         in
@@ -21,3 +49,4 @@ let content_types ?(params = false) channel =
     then inc_or_zero mime curr
     else curr                                                                  in
   foldlines add_to_set M.empty channel
+*)
