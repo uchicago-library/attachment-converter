1f314eed26564efeb5b1685dacd173aab2b1f4bc
theworldofobi <obettastephnie@gmail.com>
2023-06-29 15:15:16 -0500

    Created new file for ssconvert for spreadsheet conversion - more research needed

---
+++ conversion-scripts/ssconvert-wrapper.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+trap 'rm -rf "$TMP_DIR"' EXIT
+
+TMP_DIR=$(mktemp -d)
+
+while getopts ":i:o:" opt; do
+    case "$opt" in
+        i)
+            INPUT_EXT=$OPTARG
+            ;;
+
+        o)
+            OUTPUT_EXT=$OPTARG
+            ;;
+    esac
+done
+
+INPUT="$TMP_DIR/temp-in.$INPUT_EXT"
+OUTPUT="$TMP_DIR/temp-out.$OUTPUT_EXT"
+
+cat > $INPUT
+ssconvert [OPTIONS] $INPUT $OUTPUT
+cat $OUTPUT
