d0ef1d7e42442680b8283a1dadd55d8e85fbf741
theworldofobi <obettastephnie@gmail.com>
2023-06-29 09:43:50 -0500

    Removed files that are no longer needed

--- get-started.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-opam switch create 4.12.0 &&
-eval $(opam env) &&
-opam install -y ocamlbuild ocamlfind dune mrmime ocamlnet &&
-opam pin -y add -k hg prelude https://www.lib.uchicago.edu/keith/hg/prelude &&
-opam pin -y add mattlude https://github.com/bufordrat/mattlude.git &&
-opam pin -y add spinup https://github.com/bufordrat/spinup.git &&
-opam list
-
