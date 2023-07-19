ce0ef635b8be1c259a7085e73fde88a8c318da0a
Matt Teichman <teichman@uchicago.edu>
2023-07-19 14:54:47 -0500

    whitespace changes

--- lib/configuration.mli
+++ lib/configuration.mli
@@ -1,26 +1,23 @@
 module Formats :
   sig
     type transform_data = {
-      target_type : string;
-      target_ext : string;
-      shell_command : string;
-      convert_id : string;
+        target_type : string;
+        target_ext : string;
+        shell_command : string;
+        convert_id : string;
       }
     type t
+
     module Dict :
       sig
-        module M :
-          sig
-            type key = Prelude.String.t
-            type 'a t = 'a Stdlib__Map.Make(Prelude.String).t
-            val find : key -> 'a t -> 'a  
-          end
-
-        type key = Prelude.String.t
-        type 'a t = 'a Stdlib__Map.Make(Prelude.String).t
+        type key = string
+        type 'a t
         val find : key -> 'a t -> 'a
       end
-    val conversions : 'a list Dict.t -> Dict.key -> 'a list
+
+    val conversions : 'a list Dict.t ->
+                      Dict.key ->
+                      'a list
   end
 
 module ParseConfig :
@@ -31,14 +28,18 @@ module ParseConfig :
       | TargetExt
       | ShellCommand
       | ConvertID
+
     module Error :
       sig
         type t =
-            [ `ConfigData of int * config_key | `ReferParse of int * string ]
+          [ `ConfigData of int * config_key
+          | `ReferParse of int * string ]
         val message :
-          [< `ConfigData of int * config_key | `ReferParse of int * string ] ->
+          [< `ConfigData of int * config_key
+          | `ReferParse of int * string ] ->
           string
       end
+
     type config_entry = {
       source_type : string;
       target_type : string;
@@ -46,13 +47,20 @@ module ParseConfig :
       shell_command : string;
       convert_id : string;
     }
+
     val entry_of_assoc :
-      (string * string) list -> (config_entry, config_key) result
-    val transform_data_of_entry : config_entry -> Formats.transform_data
+      (string * string) list ->
+      (config_entry, config_key) result
+
+    val transform_data_of_entry :
+      config_entry ->
+      Formats.transform_data
+
     val parse :
       string ->
       (Formats.transform_data list Formats.Dict.t,
-       [> `ConfigData of int * config_key | `ReferParse of int * string ])
+       [> `ConfigData of int * config_key
+       | `ReferParse of int * string ])
       result
   end
 
