3006f668a339a298df97a06af65b56934a7559ac
theworldofobi <obettastephnie@gmail.com>
2023-08-02 10:17:51 -0500

    Removed unneeded shell scripts

--- make-shells/os-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if command -v pacman > /dev/null; then
-    sudo pacman -S libreoffice pandoc ghostscript gnumeric libvips catdoc
-elif command -v brew > /dev/null; then
-    brew install libreoffice pandoc ghostscript gnumeric vips verapdf
-elif command -v apt > /dev/null; then
-    sudo apt install libreoffice pandoc ghostscript gnumeric libvips-tools verapdf
-fi
