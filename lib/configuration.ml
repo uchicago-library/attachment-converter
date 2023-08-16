aae7ed92a662c6ba376764af51950e0c5fe726e0
Nathan Mull <nmmull@gmail.com>
2023-08-16 14:57:30 -0400

    Beginning of full refactor

--- lib/configuration.ml
+++ lib/configuration.ml
@@ -2,10 +2,10 @@ open Prelude
 
 module Transform_data = struct
   type t =
-    { target_type : Mime_type.t;
-      target_ext : string;
-      shell_command : string;
-      convert_id : string;
+    { target_type : Mime_type.t ;
+      target_ext : string ;
+      shell_command : string ;
+      convert_id : string ;
     }
 
   let target_type td = td.target_type
@@ -13,14 +13,11 @@ module Transform_data = struct
   let shell_command td = td.shell_command
   let convert_id td = td.convert_id
 
-  let of_record td = td
-  let to_record td = td
-
   let make ~target_type ~target_ext ~shell_command ~convert_id =
-    { target_type = target_type;
-      target_ext = target_ext;
-      shell_command = shell_command;
-      convert_id = convert_id;
+    { target_type = target_type ;
+      target_ext = target_ext ;
+      shell_command = shell_command ;
+      convert_id = convert_id ;
     }
 
   let make_no_ext ~target_type ~shell_command ~convert_id =
@@ -29,6 +26,91 @@ module Transform_data = struct
       ~target_ext:(Mime_type.extension target_type)
       ~shell_command:shell_command
       ~convert_id:convert_id
+end
+
+module Config_key = struct
+  type t = [
+    | `SourceType
+    | `TargetType
+    | `TargetExt
+    | `ShellCommand
+    | `ConvertID
+    ]
+
+  let to_string k =
+    match k with
+    | `SourceType -> "source_type"
+    | `TargetType -> "target_type"
+    | `TargetExt -> "target_ext"
+    | `ShellCommand -> "shell_command"
+    | `ConvertID -> "convert_id"
+
+end
+
+module Config_entry = struct
+  type t =
+    { source_type : string ;
+      target_type : string ;
+      target_ext : string option ;
+      shell_command: string ;
+      convert_id : string ;
+    }
+
+  let make
+        ~source_type
+        ~target_type
+        ~target_ext
+        ~shell_command
+        ~convert_id
+    = { source_type = source_type ;
+        target_type = target_type ;
+        target_ext = target_ext ;
+        shell_command = shell_command ;
+        convert_id = convert_id ;
+      }
+
+  let source_type ce = ce.source_type
+  let target_type ce = ce.target_type
+  let target_ext ce = ce.target_ext
+  let shell_command ce = ce.shell_command
+  let convert_id ce = ce.convert_id
+
+  let to_refer entry : Refer.t =
+    let open Config_key in
+    Option.(to_list (map (fun ext -> "target_ext", ext) (target_ext entry)))
+    @ [ to_string `SourceType , source_type entry ;
+        to_string `TargetType , target_type entry ;
+        to_string `ShellCommand , shell_command entry ;
+        to_string `ConvertID , convert_id entry ;
+      ]
+
+  let of_refer rentry =
+    let check key =
+      Option.to_result
+        ~none:key
+        (assoc_opt (Config_key.to_string key) rentry)
+    in
+    let ( let* ) = Result.(>>=) in
+    let* st = check `SourceType in
+    let* tt = check `TargetType in
+    let* sc = check `ShellCommand in
+    let te = Result.to_option (check `TargetExt) in
+    let* id = check `ConvertID in
+    Ok (make
+          ~source_type:st
+          ~target_type:tt
+          ~target_ext:te
+          ~shell_command:sc
+          ~convert_id:id)
+
+  let to_transform_data ce =
+    let target_mtype = Mime_type.of_string (target_type ce) in
+    let ext = Mime_type.extension target_mtype in
+    Transform_data.make
+      ~target_type:target_mtype
+      ~target_ext:ext
+      ~shell_command:(shell_command ce)
+      ~convert_id:(convert_id ce)
 end
 
 module Formats = struct
@@ -47,57 +129,52 @@ module Formats = struct
   let conversions d ct =
     Option.default []
       (Dict.find_opt ct d)
-end
 
-module ConfigEntry = struct
-  type t =
-    { source_type : Mime_type.t ;
-      target_type : Mime_type.t ;
-      target_ext : string option;
-      shell_cmd : string ;
-      convert_id : string ;
-    }
-
-  let make st tt te sc id : t =
-    { source_type = st ;
-      target_type = tt ;
-      target_ext = te ;
-      shell_cmd = sc ;
-      convert_id = id ;
-    }
-
-  let to_refer entry : Refer.t =
-    List.map
-      (fun ext -> "target_ext" , ext)
-      (Option.to_list entry.target_ext)
-    @ [ "source_type" , Mime_type.to_string entry.source_type ;
-        "target_type" , Mime_type.to_string entry.target_type ;
-        "shell_command" , entry.shell_cmd ;
-        "convert_id" , entry.convert_id ;
+  module Error = struct
+    type t = [
+      | `ConfigData of int * Config_key.t
+      | `ReferParse of int * string
       ]
+
+    let message err =
+      match err with
+      | `ConfigData (line_num, key)  ->
+         Printf.sprintf
+           "Config Data Error: (entry starting at line %d) Missing key '%s'"
+           line_num
+           (Config_key.to_string key)
+      | `ReferParse (line_num, line) ->
+         Printf.sprintf
+           "Refer Parse Error: (line %d) Cannot parse '%s'"
+           line_num
+           line
+  end
+
+  let of_string config_str =
+    let (>>=) = Result.(>>=) in
+    let insert_append k v =
+      Dict.update k (fun curr -> Some (v :: Option.value curr ~default:[]))
+    in
+    let update_accum line_num next accum =
+      Result.witherr (fun k -> `ConfigData (line_num, k)) (Config_entry.of_refer next) >>= (fun entry ->
+        Ok (insert_append (Config_entry.source_type entry) (Config_entry.to_transform_data entry) accum))
+    in
+    let collect_varieties line_num next raccum = raccum >>= update_accum line_num next in
+    let error_handler     line_num line rerror = rerror >>= (fun _ ->
+      Error (`ReferParse (line_num, line)))
+    in
+    Refer.fold
+      (Refer.witherr error_handler collect_varieties)
+      (Ok Dict.empty)
+      (Refer.of_string config_str)
 end
 
 module ParseConfig = struct
   open Formats
 
-  type config_key =
-    | SourceType
-    | TargetType
-    | TargetExt
-    | ShellCommand
-    | ConvertID
-
-  let string_of_config_key key =
-    match key with
-    | SourceType   -> "source_type"
-    | TargetType   -> "target_type"
-    | TargetExt    -> "target_extension"
-    | ShellCommand -> "shell_command"
-    | ConvertID    -> "id"
-
   module Error = struct
     type t = [
-      | `ConfigData of int * config_key
+      | `ConfigData of int * Config_key.t
       | `ReferParse of int * string
     ]
 
@@ -107,7 +184,7 @@ module ParseConfig = struct
           Printf.sprintf
             "Config Data Error: (entry starting at line %d) Missing key '%s'"
             line_num
-            (string_of_config_key key)
+            (Config_key.to_string key)
       | `ReferParse (line_num, line) ->
           Printf.sprintf
             "Refer Parse Error: (line %d) Cannot parse '%s'"
@@ -115,54 +192,14 @@ module ParseConfig = struct
             line
   end
 
-  type config_entry =
-    { source_type   : string ;
-      target_type   : string ;
-      target_ext    : string ;
-      shell_command : string ;
-      convert_id    : string ;
-    }
-
-  let entry_of_assoc config_assoc =
-    let make st tt te ss id =
-      { source_type   = st ;
-        target_type   = tt ;
-        target_ext    = te ;
-        shell_command = ss ;
-        convert_id    = id ;
-      }
-    in
-    let check key =
-      Option.to_result
-        ~none:key
-        (assoc_opt (string_of_config_key key) config_assoc)
-    in
-    let  ( let* ) = Result.(>>=)       in
-    let* st       = check SourceType   in
-    let* tt       = check TargetType   in
-    let* ss       = check ShellCommand in
-    let  te       = Result.default
-                      (Mime_type.extension (Mime_type.of_string tt))
-                      (check TargetExt)
-    in
-    let* id       = check ConvertID    in
-    Ok (make st tt te ss id)
-
-  let transform_data_of_entry entry =
-    Transform_data.make
-      ~target_type:(Mime_type.of_string entry.target_type)
-      ~target_ext:entry.target_ext
-      ~shell_command:entry.shell_command
-      ~convert_id:entry.convert_id
-
   let parse config_str =
     let (>>=) = Result.(>>=) in
     let insert_append k v =
       Dict.update k (fun curr -> Some (v :: Option.value curr ~default:[]))
     in
     let update_accum line_num next accum =
-      Result.witherr (fun k -> `ConfigData (line_num, k)) (entry_of_assoc next) >>= (fun entry ->
-        Ok (insert_append entry.source_type (transform_data_of_entry entry) accum))
+      Result.witherr (fun k -> `ConfigData (line_num, k)) (Config_entry.of_refer next) >>= (fun entry ->
+        Ok (insert_append (Config_entry.source_type entry) (Config_entry.to_transform_data entry) accum))
     in
     let collect_varieties line_num next raccum = raccum >>= update_accum line_num next in
     let error_handler     line_num line rerror = rerror >>= (fun _ ->
@@ -173,7 +210,7 @@ module ParseConfig = struct
       (Ok Dict.empty)
       (Refer.of_string config_str)
 
-  let parse_config_file = Prelude.readfile >> parse
+  let parse_config_file = Prelude.readfile >> Formats.of_string
 end
 
 let default_config () =
