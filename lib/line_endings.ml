06981113424554e80ed895634eaa11e3c5d1bd44
Matt Teichman <teichman@uchicago.edu>
2024-04-05 16:24:31 -0500

    maybe a bit nicer?

--- lib/line_endings.ml
+++ lib/line_endings.ml
@@ -3,7 +3,8 @@ type line_feed = Dos | Unix | Apple
 let check_file filepath =
   let open Prelude in
   let dipstick () = within readline filepath in
-  match (dipstick ()).[String.length (dipstick ()) - 1] with
+  let bang_bang n str = Prelude.String.get str n in
+  match String.(dipstick () |> rev |> bang_bang 0) with
   | exception End_of_file -> Apple
   | '\r' -> Dos
   | _ -> Unix
