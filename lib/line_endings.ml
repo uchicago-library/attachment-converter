1ab649b0be69c574bb3347f9e462dec3e8f431e8
Matt Teichman <teichman@uchicago.edu>
2024-04-16 09:13:52 -0500

    style change

--- lib/line_endings.ml
+++ lib/line_endings.ml
@@ -4,7 +4,9 @@ let check_file filepath =
   let open Prelude in
   let dipstick () = within readline filepath in
   let bang_bang n str = Prelude.String.get str n in
-  match String.(dipstick () |> rev |> bang_bang 0) with
+  match
+    dipstick () |> String.rev |> bang_bang 0
+  with
   | exception End_of_file -> Apple
   | '\r' -> Dos
   | _ -> Unix
