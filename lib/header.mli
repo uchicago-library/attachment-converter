5981cc54fd45bebdcfd965289451923c08e140c1
Nathan Mull <nmmull@gmail.com>
2024-07-24 16:32:16 -0400

    pruning header

--- lib/header.mli
+++ lib/header.mli
@@ -9,7 +9,7 @@ module Field : sig
       val make : ?quotes:bool -> string -> string -> t
       val map_attr : (string -> string) -> t -> t
       val map_value : (string -> string) -> t -> t
-      val of_string : string -> t option
+      val of_string : string -> t
       val to_string : t -> string
     end
 
@@ -18,22 +18,11 @@ module Field : sig
     val value : t -> string
     val params : t -> Parameter.t list
     val make : ?params:Parameter.t list -> string -> t
-    val of_string : string -> (t, Error.t) result
+    val of_string : string -> t
     val to_string : t -> string
 
     val lookup_param :
       ?quotes:bool -> string -> t -> string option
-
-    val update_value : string -> t -> t
-
-    val update_param :
-      string ->
-      (Parameter.t option -> Parameter.t option) ->
-      t ->
-      t
-
-    val add_param :
-      ?quotes:bool -> string -> string -> t -> t
   end
 
   type t
@@ -42,23 +31,12 @@ module Field : sig
   val value : t -> Value.t
   val to_assoc : t -> string * string
   val make : string -> Value.t -> t
-  val of_string : string -> (t, Error.t) result
+  val of_string : string -> t
   val to_string : t -> string
 end
 
 type t
 
-val to_list : t -> Field.t list
 val of_list : Field.t list -> t
-
-val of_assoc_list :
-  (string * string) list -> (t, Error.t) result
-
 val to_assoc_list : t -> (string * string) list
 val to_string : t -> string
-
-val update :
-  (Field.Value.t option -> Field.Value.t option) ->
-  string ->
-  t ->
-  t
