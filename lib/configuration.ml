b1aea43adb271464bfe0959a7545933d6f159154
Matt Teichman <teichman@uchicago.edu>
2024-08-08 10:33:28 -0500

    use smart constructor for config data error

--- lib/configuration.ml
+++ lib/configuration.ml
@@ -202,9 +202,11 @@ module Formats = struct
     in
     let on_ok line_num next raccum =
       let open Config_entry in
+      let open Formats_error.Smart in
       let* accum = raccum in
       let* entry =
-        Trace.with_error (`ConfigData line_num)
+        Trace.with_error
+          (config_data_err line_num)
           (of_refer next)
       in
       let* trans_data =
