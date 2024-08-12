cdd712f141f310c0f77a03299cb9fdf00bf912d1
benkim04 <benkim@uchicago.edu>
2024-08-12 14:51:02 -0500

    Adding homebrew formula + readme

---
+++ homebrew/Formula/attc.rb
@@ -0,0 +1,34 @@
+class Attc < Formula
+  desc "Tool for batch-converting attachments to preservation formats"
+  homepage "https://github.com/benkim04/attachment-converter"
+  url "https://github.com/uchicago-library/attachment-converter/archive/refs/tags/VER_NUM.tar.gz"
+  sha256 ""
+  license "GPL-2.0"
+
+  depends_on "opam" => :build
+  depends_on "ghostscript"
+  depends_on "mercurial"
+  depends_on "pandoc"
+  depends_on "verapdf"
+  depends_on "vips"
+
+  def install
+    ENV.deparallelize
+    system "make", "pkg-build"
+    bin.install "_build/default/main.exe" => "attc"
+    lib.install Dir["conversion-scripts/*"]
+  end
+
+  def caveats
+    <<~EOS
+      Attachment Converter depends on LibreOffice for some functionality.
+
+      Please install it using:
+        brew install --cask libreoffice
+    EOS
+  end
+
+  test do
+    system "true"
+  end
+end
---
+++ homebrew/README.txt
@@ -0,0 +1,1 @@
+After creating the release tag, make sure to update the ver_num on the URL to the tar ball as well as update the SHA sum
