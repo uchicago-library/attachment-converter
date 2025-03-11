bbd2cc38f512cf1ac9128944083f77f1350d396a
Matt Teichman <teichman@uchicago.edu>
2025-03-11 16:13:32 -0500

    Buffer.clear version

--- lib/mbox_simple.ml
+++ lib/mbox_simple.ml
@@ -1,36 +1,37 @@
 open Prelude
 open Result.Ops
 
-let message from chan =
-  (* read an email message *)
-  let buf = Buffer.create 1000 in
-  let rec loop fromline =
-    match readline chan with
-    | exception End_of_file -> (None, Buffer.contents buf)
-    | line ->
-      if String.prefix "From " line
-      then (Some line, Buffer.contents buf)
-      else begin
-        Buffer.add_string buf line ;
-        Buffer.add_char buf '\n' ;
-        loop fromline
-      end
-  in
-  let* fromline =
-    match from with
-    | Some line -> Ok line
-    | None -> (
-      match readline chan with
-      | exception End_of_file -> Error "missing from line"
-      | line ->
-        if not @@ String.prefix "From " line
-        then Error ("malformed From line: %s" % line)
-        else Ok line )
-  in
-  Ok (loop fromline)
-
 let mbox chan =
-  (* read an mbox *)
+  let open Result.Ops in
+  let buf = Buffer.create 1000 in
+  let message from chan =
+    (* read an email message *)
+    let rec loop fromline =
+      match readline chan with
+      | exception End_of_file -> (None, Buffer.contents buf)
+      | line ->
+        if String.prefix "From " line
+        then (Some line, Buffer.contents buf)
+        else begin
+          Buffer.add_string buf line ;
+          Buffer.add_char buf '\n' ;
+          loop fromline
+        end
+    in
+    let () = Buffer.clear buf in
+    let* fromline =
+      match from with
+      | Some line -> Ok line
+      | None -> (
+        match readline chan with
+        | exception End_of_file -> Error "missing from line"
+        | line ->
+          if not @@ String.prefix "From " line
+          then Error ("malformed From line: %s" % line)
+          else Ok line )
+    in
+    Ok (loop fromline)
+  in
   let rec loop fromline acc =
     match message fromline chan with
     | Error e ->
@@ -40,4 +41,4 @@ let mbox chan =
     | Ok (fromline, msg) ->
       loop fromline ((fromline, msg) :: acc)
   in
-  loop None []
+  loop None [] |> map snd
