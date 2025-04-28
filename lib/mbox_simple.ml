208303df3aa3529cf56959c89e03ac51d5218261
Charles Njoroge <cinjoroge@gmail.com>
2025-04-28 15:08:01 -0500

    Simplified parsing

--- lib/mbox_simple.ml
+++ lib/mbox_simple.ml
@@ -5,45 +5,118 @@ let preview str =
   let msg = String.take 50 str in
   printf "Preview:\n%s...\n" msg
 
-let mbox f chan =
-  let open Result.Ops in
-  let buf = Buffer.create 1000 in
-  let message from chan =
-    (* read an email message *)
-    let rec loop fromline =
-      match readline chan with
-      | exception End_of_file -> (None, Buffer.contents buf)
-      | line ->
-        if String.prefix "From " line
-        then (Some line, Buffer.contents buf)
-        else begin
-          Buffer.add_string buf line ;
-          Buffer.add_char buf '\n' ;
-          loop fromline
-        end
-    in
-    let () = Buffer.clear buf in
-    (* read the first line to see whether it's a From
-       line *)
-    let* fromline =
-      match from with
-      | Some line -> Ok line
-      | None -> (
-        match readline chan with
-        | exception End_of_file -> Error "missing from line"
-        | line ->
-          if not @@ String.prefix "From " line
-          then Error ("malformed From line: %s" % line)
-          else Ok line )
-    in
-    Ok (loop fromline)
-  in
-  let rec loop fromline =
-    match message fromline chan with
-    | Error e -> prerr_endline e
-    | Ok (None, msg) -> f msg
-    | Ok (fromline, msg) ->
-      f msg ;
-      loop fromline
-  in
-  loop None
+
+(* Define a channel type *)
+module type Channel = sig   
+  (* type of the input channel *)
+  type t
+  (* input line function *)
+  val input_line : t -> string
+end
+
+module type MBoxChannel = sig
+  type mchan
+  type source 
+  val peek_line : mchan -> string option * mchan
+  val read_line : mchan -> unit
+  val get_contents : mchan -> (string, string) result
+
+  val create : source -> mchan
+end
+
+module MBoxChannel (C : Channel) : MBoxChannel with type source = C.t = struct
+  
+  type source =  C.t
+  
+  type mchan = {
+    (* input channel, or a channel *)
+    chan: source;
+    (* buffer that stores the information in the channel *)
+    buffer: Buffer.t;
+    (* the line we've peeked *)
+    mutable peeked: string option
+  }
+
+  (* internal mbox channe that contains the channel, the buffer, and the peeked data *)
+  let create chan = {chan; buffer = Buffer.create 1000; peeked = None}
+
+  (* return the contents of the buffer *)
+  let get_contents m =
+    let data = Buffer.contents m.buffer in
+    Buffer.clear m.buffer; Ok data
+  
+  (* look at the next line without "consuming" it *)
+  let peek_line m =
+    match m.peeked with
+    | Some line -> Some line, m
+    | None -> match C.input_line m.chan with
+            | line -> m.peeked <- Some line; Some line, m
+            | exception End_of_file -> None, m
+
+  let read_line m = 
+    let read line = Buffer.add_string m.buffer line ; Buffer.add_char m.buffer '\n' ; in 
+    match m.peeked with
+    | Some line ->  m.peeked <- None; read line
+    | None -> 
+      match C.input_line m.chan with 
+      | line -> read line ;
+      | exception End_of_file -> raise End_of_file
+      
+end
+
+module MboxReader (MBoxChannel : MBoxChannel) = struct
+  (* given an input channel reads in an email 
+   this will also perform validation. The first line
+   for whether the channel starts should always be a valid from line
+*)
+let rec read_email (input_chan : MBoxChannel.mchan) start_of_email =
+  (* check whether a line is a from line *)  
+  let is_from_line line = String.length line >= 5 && String.sub line 0 5 = "From " in
+  (* consume the line and then move on to the next line making sure that it isi not marked as the start *)
+  let rec read_line input_chan start_of_email =
+    (* get the line from the input channel *)
+    match MBoxChannel.peek_line input_chan with (* this will be a peek *)
+    (* if you reached the end of the file, return everything you've read so far *)  
+      | None, m -> MBoxChannel.get_contents m 
+      | Some line, m -> 
+                if start_of_email && not(is_from_line line) then Error (Printf.sprintf "malformed start of email. From line: %s" line)
+                (* we have successfully read an email since we've reached another from line *)
+                else if is_from_line line && not(start_of_email) then MBoxChannel.get_contents input_chan
+                (* consume the line and continue *)
+                else (MBoxChannel.read_line m; read_line m false)
+  in read_line input_chan start_of_email
+
+let read_all_mbox chan f =
+  let rec _read_all_mbox _chan _f =
+    let mbox_channel = MBoxChannel.create chan in 
+      match read_email mbox_channel true with
+        | Error _ -> []
+        | Ok email -> f email ; _read_all_mbox _chan _f
+  in _read_all_mbox chan f
+
+  let mbox_file_fold fn inchan acc =		(* KW *)
+  let ic = MBoxChannel.create inchan in
+  let rec loop acc =
+    match read_email ic true with
+    | exception End_of_file -> acc
+    | msg -> loop (fn acc msg)
+  in loop acc
+end
+
+module FileChannel : Channel = struct
+  type t = in_channel
+  let input_line = input_line
+end
+
+module FileMBoxChannel = MBoxChannel(FileChannel)
+
+module FileMBoxReader = MboxReader(FileMBoxChannel)
+
+
+let () =
+  let ic  = open_in "mailbox.txt" in
+  let mbox_channel = FileMBoxChannel.create ic in
+  match FileMBoxReader.read_email mbox_channel true with
+  | Ok email -> preview ("Email:\n" ^ email)
+  | Error err -> preview ("Error: " ^ err)
+
---
+++ tests/mbox_simple_tests.ml
@@ -0,0 +1,74 @@
+open OUnit2
+open Lib.Mbox_simple
+
+let string_input_channel str =
+  let tmp_file = Filename.temp_file "mbox_test" ".txt" in
+  let oc = open_out tmp_file in
+  output_string oc str;
+  close_out oc;
+  open_in tmp_file
+
+let test_peek_line _ =
+  let ic = string_input_channel "From alice@example.com\nBody line 1\n" in
+  let mchan = MBoxChannel.create ic in
+  let line1, _ = MBoxChannel.peek_line mchan in
+  let line2, _ = MBoxChannel.peek_line mchan in
+  assert_equal line1 line2;
+  close_in ic
+
+let test_read_line_and_buffer _ =
+  let ic = string_input_channel "From bob@example.com\nLine 1\nLine 2\n" in
+  let mchan = MBoxChannel.create ic in
+  ignore (MBoxChannel.read_line mchan); 
+  ignore (MBoxChannel.read_line mchan); 
+  let _, _ = MBoxChannel.peek_line mchan in
+  ignore (MBoxChannel.read_line mchan);
+  match MBoxChannel.get_contents mchan with
+  | Ok contents ->
+      assert_equal "From bob@example.com\nLine 1\nLine 2\n" contents
+  | Error _ -> ();
+  close_in ic
+
+let test_malformed_from_line _ =
+  let ic = string_input_channel "Invalid start\nFrom bob@example.com\nLine\n" in
+  let mchan = MBoxChannel.create ic in
+  match read_email mchan true with
+  | Error msg -> assert_bool "should contain malformed error" (String.contains msg 'm')
+  | Ok _ -> assert_failure "Expected error due to malformed from line"
+
+let test_valid_single_email _ =
+  let ic = string_input_channel "From charlie@example.com\nHello\nWorld\nFrom delta@example.com\n" in
+  let mchan = MBoxChannel.create ic in
+  match read_email mchan true with
+  | Ok email -> assert_equal "From charlie@example.com\nHello\nWorld\n" email
+  | Error _ -> ();
+  close_in ic
+
+let test_read_all_mbox_collects_all _ =
+  let emails = ref [] in
+  let ic = string_input_channel "From a@example.com\nBody 1\nFrom b@example.com\nBody 2\nFrom c@example.com\nBody 3\n" in
+  read_all_mbox ic (fun e -> ());
+  assert_equal 3 (List.length !emails);
+  close_in ic
+
+let test_mbox_file_fold _ =
+  let ic = string_input_channel
+    "From one@example.com\nFirst message\nFrom two@example.com\nSecond message\n" in
+  let acc = mbox_file_fold (fun acc -> function
+    | Ok msg -> msg :: acc
+    | Error _ -> acc) ic [] in
+  assert_equal 2 (List.length acc);
+  assert_bool "should include 'First message'" (List.exists (fun m -> String.contains m 'F') acc);
+  close_in ic
+
+let suite =
+  "MBoxChannel Tests" >::: [
+    "peek_line" >:: test_peek_line;
+    "read_line_and_buffer" >:: test_read_line_and_buffer;
+    "malformed_from_line" >:: test_malformed_from_line;
+    "valid_single_email" >:: test_valid_single_email;
+    (* "read_all_mbox" >:: test_read_all_mbox_collects_all;
+    "mbox_file_fold" >:: test_mbox_file_fold; *)
+  ]
+
+let () = run_test_tt_main suite
