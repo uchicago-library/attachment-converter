d142c4f6bb004b6e5915e9551d3a8e0dc81c4067
Matt Teichman <teichman@uchicago.edu>
2025-06-13 09:54:10 -0500

    add slash to scripts path

--- lib/configuration.ml
+++ lib/configuration.ml
@@ -82,7 +82,7 @@ module ConvUtil = struct
   let envoke ut = ut.envoke
 
   let makefile_home_install_path =
-    Prelude.File.squiggle "~/.config/attachment-converter/scripts"
+    Prelude.File.squiggle "~/.config/attachment-converter/scripts/"
 
   let default_script_dir () =
     let paths =
@@ -90,8 +90,7 @@ module ConvUtil = struct
         "/opt/homebrew/Cellar/attc/" ^ Version.ver_num
         ^ "/lib/";
         "/usr/local/Cellar/attc/" ^ Version.ver_num
-        ^ "/lib/";
-        "/usr/lib/attachment-converter/scripts/";
+        ^ "/lib/" ;
       ]
     in
     let rec find_path = function
