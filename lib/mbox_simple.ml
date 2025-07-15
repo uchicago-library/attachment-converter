1331f38e9a3808f930a8af3990187cf00d5d7f9b
Nathan Mull <nmmull@gmail.com>
2025-07-15 10:44:45 -0400

    updates to syntax

--- lib/mbox_simple.ml
+++ lib/mbox_simple.ml
@@ -1,36 +1,33 @@
 (* open Prelude *)
 
-type mbox_io = {
-  mutable from_line : string option;
-  chan : in_channel;
-}
+type mbox_io =
+  { mutable from_line : string option; chan : in_channel }
 
-let is_from_line line = String.length line >= 5 && String.sub line 0 5 = "From "
+let is_from_line line =
+  String.length line >= 5 && String.sub line 0 5 = "From "
 
-let check_mbox (chan : in_channel) : mbox_io option = assert false
+let check_mbox (chan : in_channel) : mbox_io option =
+  assert false
 
-let read_email (buffer : Buffer.t) (m : mbox_io) : (string * string) option =
+let read_email (buffer : Buffer.t) (m : mbox_io) :
+    (string * string) option =
   let rec go () =
     match m.from_line with
     | None -> None
     | Some from_line -> (
-        match In_channel.input_line m.chan with
-        | None -> (
-            m.from_line <- None;
-            Some (from_line, Buffer.contents buffer)
-          )
-        | Some next_line ->
-          if is_from_line next_line
-          then (
-            m.from_line <- Some next_line;
-            Some (from_line, Buffer.contents buffer)
-          )
-          else (
-            Buffer.add_string buffer next_line;
-            Buffer.add_char buffer '\n';
-            go ()
-          )
-      )
+      match In_channel.input_line m.chan with
+      | None ->
+        m.from_line <- None ;
+        Some (from_line, Buffer.contents buffer)
+      | Some next_line ->
+        if is_from_line next_line
+        then (
+          m.from_line <- Some next_line ;
+          Some (from_line, Buffer.contents buffer) )
+        else (
+          Buffer.add_string buffer next_line ;
+          Buffer.add_char buffer '\n' ;
+          go () ) )
   in
   go ()
 
@@ -41,96 +38,105 @@ module type Parser = sig
   (* create an internal channel *)
   val create_mchan : in_channel -> mchan
 
-  val fold : ('a -> (string, string) result -> 'a) -> in_channel -> 'a -> 'a
+  val fold :
+    ('a -> (string, string) result -> 'a) ->
+    in_channel ->
+    'a ->
+    'a
 
   (* read a single entity *)
   val read : mchan -> bool -> (string, string) result
 
   (* convert messages in channel by applying f*)
-  val convert : in_channel -> (string -> (string, 'err) result) -> (unit, 'err) result
-
+  val convert :
+    in_channel ->
+    (string -> (string, 'err) result) ->
+    (unit, 'err) result
 end
 
-(*
-  Creates an mbox parser
-*)
+(* Creates an mbox parser *)
 module MBoxParser : Parser = struct
-
   (* a channel wrapper for an mbox *)
-  type mchan = {
-    (* channel that contains the data *)
-    chan: in_channel;
+  type mchan =
+    { (* channel that contains the data *)
+      chan : in_channel;
+      (* buffer to hold the contents of the channel *)
+      buffer : Buffer.t;
+      (* next line in the channel *)
+      mutable next : string option;
+      (* current line number *)
+      mutable line_num : int
+    }
 
-    (* buffer to hold the contents of the channel *)
-    buffer: Buffer.t;
-
-    (* next line in the channel *)
-    mutable next: string option;
-
-    (* current line number *)
-    mutable line_num: int;
-  }
-
-  (* create an mbox channel with a buffer, an empty peek option, and initial line number *)
+  (* create an mbox channel with a buffer, an empty peek
+     option, and initial line number *)
   let create_mchan chan =
-    { chan; buffer = Buffer.create 1000; next = None; line_num = 0 }
+    { chan;
+      buffer = Buffer.create 1000;
+      next = None;
+      line_num = 0
+    }
 
   (* return the current line number *)
   let current_line_number m = m.line_num
 
   (* get the contents of the buffer *)
   let get_contents m =
-    let contents = Buffer.contents m.buffer in Buffer.clear m.buffer;
+    let contents = Buffer.contents m.buffer in
+    Buffer.clear m.buffer ;
     Ok contents
 
   (* look and store the next line *)
   let peek_line m =
     match m.next with
-    | Some line -> Some line, m
+    | Some line -> (Some line, m)
     | None ->
       let result =
         try
           let line = input_line m.chan in
-          m.next <- Some line;
-          m.line_num <- m.line_num + 1;
+          m.next <- Some line ;
+          m.line_num <- m.line_num + 1 ;
           Some line
         with End_of_file -> None
-      in result, m
+      in
+      (result, m)
 
   let read_line m =
     let record line =
-      Buffer.add_string m.buffer line;
+      Buffer.add_string m.buffer line ;
       Buffer.add_char m.buffer '\n'
     in
     match m.next with
     | Some line ->
-        m.next <- None;
-        record line
+      m.next <- None ;
+      record line
     | None ->
-        let line = input_line m.chan in
-        m.line_num <- m.line_num + 1;
-        record line
+      let line = input_line m.chan in
+      m.line_num <- m.line_num + 1 ;
+      record line
 
-  let is_from_line line = String.length line >= 5 && String.sub line 0 5 = "From "
+  let is_from_line line =
+    String.length line >= 5 && String.sub line 0 5 = "From "
 
   let rec read mbox start_of_email =
-  match peek_line mbox with
-  | None, _ ->
-      if Buffer.length mbox.buffer > 0 then
-        get_contents mbox
-      else
-        Error "End of file"
-  | Some line, m ->
-      if start_of_email && not (is_from_line line) then
-        Error (Printf.sprintf "Malformed start of email at line %d: %s" (current_line_number m) line)
-      else if is_from_line line && not start_of_email then
-        get_contents m
+    match peek_line mbox with
+    | None, _ ->
+      if Buffer.length mbox.buffer > 0
+      then get_contents mbox
+      else Error "End of file"
+    | Some line, m ->
+      if start_of_email && not (is_from_line line)
+      then
+        Error
+          (Printf.sprintf
+             "Malformed start of email at line %d: %s"
+             (current_line_number m)
+             line )
+      else if is_from_line line && not start_of_email
+      then get_contents m
       else (
-        read_line m;
-        read m false
-      )
-
-
+        read_line m ;
+        read m false )
 
   let fold fn chan acc =
     let mbox = create_mchan chan in
@@ -142,49 +148,44 @@ module MBoxParser : Parser = struct
     in
     loop acc
 
-    let convert chan (f : string -> (string, 'err) result) : (unit, 'err) result =
-  fold
-    (fun acc msg ->
-      match acc with
-      | Error e -> Error e
-      | Ok () ->
-        match msg with
-        | Error parse_err ->
-            prerr_endline ("Parse error: " ^ parse_err);
+  let convert chan (f : string -> (string, 'err) result) :
+      (unit, 'err) result =
+    fold
+      (fun acc msg ->
+        match acc with
+        | Error e -> Error e
+        | Ok () -> (
+          match msg with
+          | Error parse_err ->
+            prerr_endline ("Parse error: " ^ parse_err) ;
             Ok ()
-        | Ok s -> (
+          | Ok s -> (
             match f s with
             | Ok transformed ->
-                output_string stdout transformed;
-                flush stdout;
-                Ok ()
-            | Error err -> Error err
-        )
-    )
-    chan (Ok ())
-
+              output_string stdout transformed ;
+              flush stdout ;
+              Ok ()
+            | Error err -> Error err ) ) )
+      chan (Ok ())
 end
 
 let () =
   let input_file =
-    if Array.length Sys.argv > 1 then Sys.argv.(1)
-    else "-"
+    if Array.length Sys.argv > 1 then Sys.argv.(1) else "-"
   in
   let ic =
-    if input_file = "-" then stdin
-    else open_in input_file
+    if input_file = "-" then stdin else open_in input_file
   in
-
   let result =
     MBoxParser.convert ic (fun msg ->
-      Ok ("--- EMAIL START ---\n" ^ msg ^ "--- EMAIL END ---\n\n"))
+        Ok
+          ( "--- EMAIL START ---\n" ^ msg
+          ^ "--- EMAIL END ---\n\n" ) )
   in
-
   (* Close input file if it's not stdin *)
-  if input_file <> "-" then close_in ic;
-
+  if input_file <> "-" then close_in ic ;
   match result with
   | Ok () -> ()
   | Error err ->
-      prerr_endline ("Error: " ^ err);
-      exit 1
+    prerr_endline ("Error: " ^ err) ;
+    exit 1
--- main.ml
+++ main.ml
@@ -5,55 +5,58 @@
  * %%NAME%% %%VERSION%%
  *)
 
- open Prelude
- open Cmdliner
- 
- module Data = struct
-   module Printer = struct
-     let print msg = write stdout msg
-   end
- end
- 
- type cmd_input = [`Stdin | `File of string]
- 
- type cmd_input_parser =
-   string -> (cmd_input, [`Msg of string]) Stdlib.result
- 
- type cmd_input_printer = cmd_input Arg.printer
- 
- let cmd_input_parser str =
-   if Sys.file_exists str
-   then Ok (`File str)
-   else Error (`Msg ("File: " ^ str ^ " does not exist."))
- 
- let cmd_input_printer fmt input =
-   let str =
-     match input with
-     | `Stdin -> "STDIN"
-     | `File fn -> fn
-   in
-   Format.pp_print_string fmt str
- 
- let cmd_input_conv =
-   Arg.conv (cmd_input_parser, cmd_input_printer)
- 
- let report ?(params = false) ic =
-   let open Lib.Report in
-   let module M = Map.Make (String) in
-   let types = content_types ~params ic in
-   print "Content Types:" ;
-   M.iter
-     (fun k v -> print ("  " ^ k ^ " : " ^ Int.to_string v))
-     types
- 
-let convert config_files ?(single_email = false) ic pbar backend =
+open Prelude
+open Cmdliner
+
+module Data = struct
+  module Printer = struct
+    let print msg = write stdout msg
+  end
+end
+
+type cmd_input = [`Stdin | `File of string]
+
+type cmd_input_parser =
+  string -> (cmd_input, [`Msg of string]) Stdlib.result
+
+type cmd_input_printer = cmd_input Arg.printer
+
+let cmd_input_parser str =
+  if Sys.file_exists str
+  then Ok (`File str)
+  else Error (`Msg ("File: " ^ str ^ " does not exist."))
+
+let cmd_input_printer fmt input =
+  let str =
+    match input with
+    | `Stdin -> "STDIN"
+    | `File fn -> fn
+  in
+  Format.pp_print_string fmt str
+
+let cmd_input_conv =
+  Arg.conv (cmd_input_parser, cmd_input_printer)
+
+let report ?(params = false) ic =
+  let open Lib.Report in
+  let module M = Map.Make (String) in
+  let types = content_types ~params ic in
+  print "Content Types:" ;
+  M.iter
+    (fun k v -> print ("  " ^ k ^ " : " ^ Int.to_string v))
+    types
+
+let convert config_files ?(single_email = false) ic pbar
+    backend =
   let b =
     let open Lib.Backend in
     match backend with
     | Mrmime ->
-      (module Lib.Convert.Mrmime_Converter : Lib.Convert.CONVERTER)
+      ( module Lib.Convert.Mrmime_Converter
+      : Lib.Convert.CONVERTER )
     | Ocamlnet ->
-      (module Lib.Convert.Ocamlnet_Converter : Lib.Convert.CONVERTER)
+      ( module Lib.Convert.Ocamlnet_Converter
+      : Lib.Convert.CONVERTER )
   in
   let module B = (val b) in
   let open B in
@@ -61,133 +64,130 @@ let convert config_files ?(single_email = false) ic pbar backend =
   let open Lib.Mbox.ToOutput.Make (B) in
   let open Lib.Error_message in
   let ( let* ) = Result.( >>= ) in
-
   let processed =
     let open Lib.Dependency in
     let* _ = checkDependencies () in
     let* config = get_config config_files in
-    if single_email then (
+    if single_email
+    then
       let module DP = Data.Printer in
       let* converted = acopy_email config (read ic) pbar in
       let _ = DP.print converted in
       Ok ()
-    ) else (
+    else
       let handler msg =
         match acopy_email config msg pbar with
         | Ok converted -> Ok (converted ^ "\n")
         | Error errlist -> Error errlist
       in
       Lib.Mbox_simple.MBoxParser.convert ic handler
-    )
   in
-
   match processed with
   | Error err -> write stderr (message err)
   | Ok _ -> ()
 
-
- 
- let convert_wrapper config_files sem rpt rpt_p inp backend =
-   let pbar =
-     match open_out "/dev/tty" with
-     (* no controlling tty *)
-     | exception _ -> open_out "/dev/null"
-     | other -> other
-   in
-   let report_or_convert ic =
-     if rpt_p
-     then report ~params:true ic
-     else if rpt
-     then report ic
-     else
-       convert config_files ~single_email:sem ic pbar backend
-   in
-   match inp with
-   | `File fn -> let ic = open_in fn in
+let convert_wrapper config_files sem rpt rpt_p inp backend =
+  let pbar =
+    match open_out "/dev/tty" with
+    (* no controlling tty *)
+    | exception _ -> open_out "/dev/null"
+    | other -> other
+  in
+  let report_or_convert ic =
+    if rpt_p
+    then report ~params:true ic
+    else if rpt
+    then report ic
+    else
+      convert config_files ~single_email:sem ic pbar backend
+  in
+  match inp with
+  | `File fn ->
+    let ic = open_in fn in
     report_or_convert ic
-   | `Stdin -> report_or_convert stdin
- 
- let backend_t =
-   let open Lib.Backend in
-   let doc =
-     "Choose between 'ocamlnet' and 'mrmime' as the two \
-      possible email parsing backends."
-   in
-   let docv = "BACKEND" in
-   let backends =
-     [ ("ocamlnet", Ocamlnet); ("mrmime", Mrmime) ]
-   in
-   Arg.(
-     value
-     & opt (enum backends) Ocamlnet
-     & info [ "backend" ] ~doc ~docv )
- 
- let input_t =
-   let doc = "Input file to be converted." in
-   Arg.(value & pos 0 cmd_input_conv `Stdin & info [] ~doc)
- 
- let report_params_t =
-   let doc =
-     "Prints a list of all MIME types in the input along with\n\
-      all header and field parameters that go with it."
-   in
-   Arg.(value & flag & info [ "report-params" ] ~doc)
- 
- let report_t =
-   let doc =
-     "Provides a list of all attachment types in a given \
-      mailbox."
-   in
-   Arg.(value & flag & info [ "r"; "report" ] ~doc)
- 
- let single_email_t =
-   let doc =
-     "Converts email attachments assuming the input is a \
-      single plain text email."
-   in
-   Arg.(value & flag & info [ "single-email" ] ~doc)
- 
- let config_t =
-   let doc =
-     "Sets the absolute path $(docv) to be checked for a \
-      configuration file."
-   in
-   Arg.(
-     value
-     & opt_all file []
-     & info [ "config" ] ~docv:"PATH" ~doc )
- 
- let convert_t =
-   Term.(
-     const convert_wrapper
-     $ config_t
-     $ single_email_t
-     $ report_t
-     $ report_params_t
-     $ input_t
-     $ backend_t )
- 
- let cmd =
-   let doc = "Converts email attachments." in
-   let open Lib.Version in
-   let info = Cmd.info "attc" ~version:ver_num ~doc in
-   Cmd.v info convert_t
- 
- let main () = exit (Cmd.eval cmd)
- let () = main ()
- 
- (*
-  * Copyright (c) 2021 Matt Teichman
-  *
-  * Permission to use, copy, modify, and/or distribute this software for any
-  * purpose with or without fee is hereby granted, provided that the above
-  * copyright notice and this permission notice appear in all copies.
-  *
-  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  *)
+  | `Stdin -> report_or_convert stdin
+
+let backend_t =
+  let open Lib.Backend in
+  let doc =
+    "Choose between 'ocamlnet' and 'mrmime' as the two \
+     possible email parsing backends."
+  in
+  let docv = "BACKEND" in
+  let backends =
+    [ ("ocamlnet", Ocamlnet); ("mrmime", Mrmime) ]
+  in
+  Arg.(
+    value
+    & opt (enum backends) Ocamlnet
+    & info [ "backend" ] ~doc ~docv )
+
+let input_t =
+  let doc = "Input file to be converted." in
+  Arg.(value & pos 0 cmd_input_conv `Stdin & info [] ~doc)
+
+let report_params_t =
+  let doc =
+    "Prints a list of all MIME types in the input along with\n\
+     all header and field parameters that go with it."
+  in
+  Arg.(value & flag & info [ "report-params" ] ~doc)
+
+let report_t =
+  let doc =
+    "Provides a list of all attachment types in a given \
+     mailbox."
+  in
+  Arg.(value & flag & info [ "r"; "report" ] ~doc)
+
+let single_email_t =
+  let doc =
+    "Converts email attachments assuming the input is a \
+     single plain text email."
+  in
+  Arg.(value & flag & info [ "single-email" ] ~doc)
+
+let config_t =
+  let doc =
+    "Sets the absolute path $(docv) to be checked for a \
+     configuration file."
+  in
+  Arg.(
+    value
+    & opt_all file []
+    & info [ "config" ] ~docv:"PATH" ~doc )
+
+let convert_t =
+  Term.(
+    const convert_wrapper
+    $ config_t
+    $ single_email_t
+    $ report_t
+    $ report_params_t
+    $ input_t
+    $ backend_t )
+
+let cmd =
+  let doc = "Converts email attachments." in
+  let open Lib.Version in
+  let info = Cmd.info "attc" ~version:ver_num ~doc in
+  Cmd.v info convert_t
+
+let main () = exit (Cmd.eval cmd)
+let () = main ()
+
+(*
+ * Copyright (c) 2021 Matt Teichman
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
