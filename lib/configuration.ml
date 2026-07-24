2f7c6b773e8432c8fccea5b1bbee590a8c6a3e7f
Matt Teichman <teichman@uchicago.edu>
2026-07-24 12:30:03 -0500

    gah, it's libexec, not libexe

--- lib/configuration.ml
+++ lib/configuration.ml
@@ -99,9 +99,9 @@ module ConvUtil = struct
       [ makefile_home_install_path;
         linux_multiuser_install_path;
         "/opt/homebrew/Cellar/attc/" ^ Version.full_version
-        ^ "/libexe/";
+        ^ "/libexec/";
         "/usr/local/Cellar/attc/" ^ Version.full_version
-        ^ "/libexe/" ;
+        ^ "/libexec/" ;
       ]
     in
     let rec find_path = function
